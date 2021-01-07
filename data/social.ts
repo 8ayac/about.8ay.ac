cd904d6f50694e3df6d725cf439c844bb29b9cff
8ayac <8ayac.y@gmail.com>
2021-01-07 15:42:42 +0900

    :wrench: Modify description of each data.

--- data/social.ts
+++ data/social.ts
@@ -1,11 +1,11 @@
 import { SocialInformation } from '@src/types';
 export const social: SocialInformation[] = [
-  { description: '8ayac', url: 'https://twitter.com/8ayac' },
-  { description: '8ayac', url: 'https://hackerone.com/8ayac' },
-  { description: '8ayac', url: 'https://github.com/8ayac' },
+  { description: 'Twitter', url: 'https://twitter.com/8ayac' },
+  { description: 'HackerOne', url: 'https://hackerone.com/8ayac' },
+  { description: 'GitHub', url: 'https://github.com/8ayac' },
   { description: 'Blog', url: 'https://caya8.hatenablog.com/' },
   {
-    description: '8ayac',
+    description: 'BugBounty.jp',
     url: 'https://bugbounty.jp/users/7BgpyIvzQWQXFbmBMY0Jfpse',
   },
 ];
