66ff0bd4651a3bef3bcf7b50da465598ecea35e4
8ayac <8ayac.y@gmail.com>
2021-01-08 07:58:03 +0900

    :recycle: Export LinkWithDescription type.

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
 import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
 
-type LinkWithDescription = {
+export type LinkWithDescription = {
   url: string;
   description?: string;
 };
