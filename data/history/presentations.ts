7b4a3d3c0c7475e5811cd29fb1f0565b99f73af7
8ayac <8ayac.y@gmail.com>
2021-01-08 11:26:28 +0900

    :wrench: Add image paths.

--- data/history/presentations.ts
+++ data/history/presentations.ts
@@ -5,17 +5,20 @@ export const presentations: PresentationHistory[] = [
     title: 'MBSD Cybersecurity Challenges 2017 最終審査会 発表スライド',
     url:
       'https://speakerdeck.com/8ayac/mbsd-cybersecurity-challenges-2017-zui-zhong-shen-cha-hui-fa-biao-suraido',
+    imagePath: '/img/presentation/20171213-MBSD-final.jpg',
     publishedAt: '2017-12-13',
   },
   {
     title: 'MBSD Cybersecurity Challenges 2018 最終審査会 発表スライド',
     url:
       'https://speakerdeck.com/8ayac/mbsd-cybersecurity-challenges-2018-zui-zhong-shen-cha-hui-fa-biao-suraido',
+    imagePath: '/img/presentation/20181212-MBSD-final.jpg',
     publishedAt: '2018-12-12',
   },
   {
     title: 'Free Bugs Campaign',
     url: 'https://speakerdeck.com/8ayac/free-bugs-campaign',
+    imagePath: '/img/presentation/20190311-Burp.jpg',
     publishedAt: '2019-03-11',
   },
 ];
