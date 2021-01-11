7f607aa75a5283b8ba61177dc0ff68be557cacaf
8ayac <8ayac.y@gmail.com>
2021-01-11 10:54:47 +0900

    :ambulance: Fix the unsupported definition of DateString.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
 
-export type DateString = `${number}-${number}-${number}`;
+export type DateString = string; // /^[0-9]{4}/(0[1-9]|1[0-2])/(0[1-9]|[12][0-9]|3[01])$/
 type Term = {
   startedAt: DateString;
   endedAt?: DateString;
