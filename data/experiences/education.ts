41123838430e97d1534d98c1e5444c81618e0590
8ayac <8ayac.y@gmail.com>
2021-01-18 07:15:41 +0900

    :page_facing_up: Add school information.

--- data/experiences/education.ts
+++ data/experiences/education.ts
@@ -7,6 +7,12 @@ export const education: EducationalExperience[] = [
     school: 'Information Science College',
     location: 'Kanagawa',
     summaries: ['Currently studying as a student.'],
+    references: [
+      {
+        url: 'http://isc.iwasaki.ac.jp/index.html',
+        description: 'School information',
+      },
+    ],
     date: { startedAt: '2017-04-01' },
   },
 ];
