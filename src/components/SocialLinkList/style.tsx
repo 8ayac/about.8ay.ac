eed71dbec54cb021b5c8cd09f15dca6cf680bf53
8ayac <8ayac.y@gmail.com>
2021-01-18 04:45:35 +0900

    :art: Modify the padding size of socialLink. To improve the mobile SEO score in lighthouse.

--- src/components/SocialLinkList/style.tsx
+++ src/components/SocialLinkList/style.tsx
@@ -11,7 +11,7 @@ export const linkListWrapper = css`
 `;
 
 export const socialLink = css`
-  padding: 0.25rem 0;
+  padding: 0.5rem 0;
   margin-right: 2.8rem;
   font-size: 1.8rem;
   text-decoration: none;
