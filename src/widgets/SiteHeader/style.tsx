d049da9dfa675094038c793cfb05676ed0acb878
8ayac <8ayac.y@gmail.com>
2021-01-18 02:56:13 +0900

    :art: Modify the styles of SiteHeader component.

--- src/widgets/SiteHeader/style.tsx
+++ src/widgets/SiteHeader/style.tsx
@@ -1,11 +1,17 @@
 import { css } from '@emotion/react';
+import { mq } from '@src/shared/styles/MediaQuery';
 
 export const header = css`
-  margin-top: 3rem;
-  margin-bottom: 5rem;
+  margin-top: 10%;
+  margin-bottom: 5%;
 `;
 
 export const siteTitle = css`
-  margin: 1rem auto;
+  margin: 3rem auto;
   font-size: 5rem;
+  line-height: 1.25;
+
+  ${mq('sm')} {
+    margin: 10% auto;
+  }
 `;
