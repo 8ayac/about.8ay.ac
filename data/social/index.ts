96c5cbca641028831dc294630cb6830c4fecbdb6
8ayac <8ayac.y@gmail.com>
2021-03-02 22:30:49 +0900

    :page_facing_up: Change url of my blog.

--- data/social/index.ts
+++ data/social/index.ts
@@ -4,7 +4,7 @@ export const social: SocialInformation[] = [
   { description: 'Twitter', url: 'https://twitter.com/8ayac' },
   { description: 'HackerOne', url: 'https://hackerone.com/8ayac' },
   { description: 'GitHub', url: 'https://github.com/8ayac' },
-  { description: 'Blog', url: 'https://caya8.hatenablog.com/' },
+  { description: 'Blog', url: 'https://blog.8ay.ac' },
   {
     description: 'BugBounty.jp',
     url: 'https://bugbounty.jp/users/7BgpyIvzQWQXFbmBMY0Jfpse',
