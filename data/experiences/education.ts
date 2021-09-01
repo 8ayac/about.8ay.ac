83a6cfc898b026c8e7acc8b245c15b27e7d297d0
8ayac(Yoshinori Hayashi) <8ayac.y+github@gmail.com>
2021-09-02 01:38:28 +0900

    :memo: Update educational experience.

--- data/experiences/education.ts
+++ data/experiences/education.ts
@@ -2,17 +2,21 @@ import { EducationalExperience } from '@src/types';
 
 export const education: EducationalExperience[] = [
   {
-    degree: 'Undergraduate Student',
+    degree: '',
     department: 'Information Security',
     school: 'Information Science College',
     location: 'Kanagawa',
-    summaries: ['Currently studying as a student.'],
+    summaries: ['Received Advanced Diploma(Applied Professional Postsecondary Course(Technology))'],
     references: [
       {
         url: 'http://isc.iwasaki.ac.jp/index.html',
         description: 'School information',
       },
+      {
+        url: 'https://en.wikipedia.org/wiki/Advanced_Diploma_(Japan)',
+        description: 'Advanced Diploma (Japan)',
+      },
     ],
-    date: { startedAt: '2017-04-01' },
+    date: { startedAt: '2017-04-01', endedAt: '2021-03-31' },
   },
 ];
