ad2a5b2e3a1b3f1aa9b902aa8ab5f1d6c9a82e6c
8ayac(Yoshinori Hayashi) <8ayac.y+github@gmail.com>
2022-03-28 19:31:20 +0900

    :memo: Update experiences.

--- data/experiences/others.ts
+++ data/experiences/others.ts
@@ -11,7 +11,10 @@ export const others: OtherExperience[] = [
   },
   {
     title: 'ISCCTF 2020',
-    summaries: ['Organized ISCCTF 2020.'],
+    summaries: [
+        'Organized ISCCTF 2020.',
+        'Made some web challenges. (Greetinjs, mark damn it)'
+    ],
     references: [
       { url: 'https://caya8.hatenablog.com/entry/2020/10/28/080123' },
     ],
