e68082a5d1622cbc15e9a4489015c14b780cae96
8ayac(Yoshinori Hayashi) <8ayac.y+github@gmail.com>
2024-04-11 01:06:49 +0900

    Update data.

--- data/social/index.ts
+++ data/social/index.ts
@@ -2,7 +2,6 @@ import { SocialInformation } from '@src/types';
 
 export const social: SocialInformation[] = [
   { description: 'X', url: 'https://x.com/8ayac' },
-  { description: 'Bluesky', url: 'https://bsky.app/profile/8ay.ac' },
   { description: 'HackerOne', url: 'https://hackerone.com/8ayac' },
   { description: 'GitHub', url: 'https://github.com/8ayac' },
   { description: 'Blog', url: 'https://blog.8ay.ac' },
@@ -15,4 +14,5 @@ export const social: SocialInformation[] = [
     url: 'https://www.linkedin.com/in/yoshinori-hayashi-102656149',
   },
   { description: 'Wantedly', url: 'https://www.wantedly.com/users/36919922' },
+  { description: 'Bluesky (for more casual purpose)', url: 'https://bsky.app/profile/8ay.ac' },
 ];
